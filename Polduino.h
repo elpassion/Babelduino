9760280f093f715e618d128f76c2671c668d6794
Karol Sarnacki <karol.sarnacki@elpassion.com>
2017-06-03 23:44:14 +0200

    Add translations generator

---
+++ Polduino.h
@@ -0,0 +1,24 @@
+#ifndef POLDUINO_H
+#define POLDUINO_H
+
+#define niech auto
+#define procedura void
+#define nic void
+#define czekaj delay
+#define stop break
+#define w_przypadku case
+#define wykonuj do
+#define w_przeciwnym_razie else
+#define powtorz for
+#define jezeli if
+#define wynik return
+#define skocz_do goto
+#define wybierz switch
+#define dopoki while
+#define ustaw setup
+#define powtarzaj loop
+#define nie not
+#define lub or
+#define i and
+
+#endif /* POLDUINO_H */
