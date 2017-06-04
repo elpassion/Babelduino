6507fc091eb3f758511a0a310c255bba4db2b539
Karol Sarnacki <karol.sarnacki@elpassion.com>
2017-06-04 02:43:02 +0200

    Refactor Locale Generator

--- src/Babelduino.h
+++ src/Babelduino.h
@@ -1,24 +1,6 @@
-#ifndef POLDUINO_H
-#define POLDUINO_H
+#ifndef BABELDUINO_H
+#define BABELDUINO_H
 
-#define niech auto
-#define procedura void
-#define nic void
-#define czekaj delay
-#define stop break
-#define w_przypadku case
-#define wykonuj do
-#define w_przeciwnym_razie else
-#define powtorz for
-#define jezeli if
-#define wynik return
-#define skocz_do goto
-#define wybierz switch
-#define dopoki while
-#define ustaw setup
-#define powtarzaj loop
-#define nie not
-#define lub or
-#define i and
+#include "Babelduino_PL.h"
 
-#endif /* POLDUINO_H */
+#endif /* BABELDUINO_H */
---
+++ src/Babelduino_PL.h
@@ -0,0 +1,24 @@
+#ifndef BABELDUINO_PL_H
+#define BABELDUINO_PL_H
+
+#define niech auto
+#define procedura void
+#define nic void
+#define czekaj delay
+#define stop break
+#define w_przypadku case
+#define wykonuj do
+#define w_przeciwnym_razie else
+#define powtorz for
+#define jezeli if
+#define wynik return
+#define skocz_do goto
+#define wybierz switch
+#define dopoki while
+#define ustaw setup
+#define powtarzaj loop
+#define nie not
+#define lub or
+#define i and
+
+#endif /* BABELDUINO_PL_H */
